fa3e2e9a15dd7bec93e9fa8c74467a849cd482d6
Rosbel Sanroman <rlsanroman@tamu.edu>
2013-03-06 16:52:51 -0600

    Populate Board

--- src/Board.java
+++ src/Board.java
@@ -2,10 +2,37 @@ package twelve.team;
 
 public class Board
 {
-	private int BoardSize;
-	
+	private Piece[][] board;
 	public Board()
 	{
-		
+		board = new Piece[5][9];
+		ResetBoard();
+	}
+	
+	private void ResetBoard()
+	{
+		for(int i = 0; i < board.length; i++)
+		{
+			for(int j = 0; j < board[i].length; j++)
+			{
+				if (i == 0 || i == 1)
+				{
+					board[i][j] = new Piece(Piece.Team.BLACK);
+				}
+				else if( i == 3 || i == 4)
+				{
+					board[i][j] = new Piece(Piece.Team.WHITE);
+				}
+				else
+				{
+					if (j == 4) // middle piece
+						board[i][j] = null;
+					else if(j == 0 || j == 2 || j == 5 || j == 7) // black pieces
+						board[i][j] = new Piece(Piece.Team.BLACK);
+					else //	white pieces
+						board[i][j] = new Piece(Piece.Team.WHITE);
+				}
+			}
+		}
 	}
 }
