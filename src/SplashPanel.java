e849cf369e57216734b7a541168369b3e94f2bea
Rosbel Sanroman <rlsanroman@tamu.edu>
2013-03-18 16:37:03 -0500

    SplashScreen

---
+++ src/SplashPanel.java
@@ -0,0 +1,37 @@
+package twelve.team;
+
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class SplashPanel extends JFrame
+{
+	
+	private BufferedImage img;
+	private JLabel label;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6174475639324933305L;
+	
+	public SplashPanel()
+	{
+		try {
+			img = ImageIO.read(new File("imgs/SplashScreen.jpg"));
+			label = new JLabel();
+			label.setIcon(new ImageIcon(img));
+			this.add(label);
+			this.pack();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
