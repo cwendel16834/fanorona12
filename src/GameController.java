7ef3632c73c77f154c770e8717b8630e65eedf88
Curtis Wendel <cwendel16834@gmail.com>
2013-03-28 15:46:02 -0500

    cleanup

--- src/GameController.java
+++ src/GameController.java
@@ -2,13 +2,6 @@ package twelve.team;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 //class for managing gameplay and game logic
 //receives move info from vBoard and processes
