77d1f8819fff72ce544ac60b9e05a1e210f89286
mjmjelde <mjmjelde@gmail.com>
2013-04-02 16:41:03 -0500

    Fixed sacrifice moves.  stared getTeamMoves

--- src/Board.java
+++ src/Board.java
@@ -128,9 +128,18 @@ public class Board
 		return board;
 	}
 	
-	public void nextMove(){
+	public void nextTurn(){
 		MultipleCaptureStart = null;
 		lastMovedPiece = null;
+		for(int i=0;i<board.length;i++){
+			for(int j=0;j<board[i].length;j++){
+				if(board[i][j] != null && board[i][j].sacrificed && board[i][j].nextTurn){
+					board[i][j] = null;
+				} else if(board[i][j] != null && board[i][j].sacrificed){
+					board[i][j].nextTurn = true;
+				}
+			}
+		}
 	}
 	
 	
@@ -441,6 +450,22 @@ public class Board
 			Point newPoint = getPoint(loc, dir);
 			if (newPoint != null && isValid(loc, newPoint)){
 				//moves.add(new Move(loc, newPoint, ))
+				if(isPaika(loc, newPoint)){
+					moves.add(new Move(loc, newPoint, moveType.PAIKA));
+				}
+				Point target;
+				try{
+					target = possibleCapture(loc, newPoint, moveType.NONE);
+					if(getDirection(loc, target) == getDirection(loc, newPoint)){
+						moves.add(new Move(loc, newPoint, moveType.ADVANCE));
+					} else {
+						moves.add(new Move(loc, newPoint, moveType.RETREAT));
+					}
+				} catch(Exception e){
+					//both targets are possible
+					moves.add(new Move(loc, newPoint, moveType.ADVANCE));
+					moves.add(new Move(loc, newPoint, moveType.RETREAT));
+				}
 			}
 		}
 		
@@ -448,6 +473,14 @@ public class Board
 	}
 	
 	public ArrayList<Move> getTeamMoves(Team team) {
+		
+		ArrayList<Move> moves = new ArrayList<Move>();
+		
+		for(int i=0;i<board.length;i++){
+			for(int j=0;j<board[i].length;j++){
+				
+			}
+		}
 		
 		ArrayList<Piece> pieces = getTeamPieces(team);
 		
